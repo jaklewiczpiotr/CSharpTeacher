ab23a4409ba7d3a93e1a6c32ba58ae29e20190be
Piotr Jot <Piotr Jot>
2016-12-14 20:30:34 +0100

    Add simple test exercises

--- CSharpTeacher/Controllers/SimpleExercisesController.cs
+++ CSharpTeacher/Controllers/SimpleExercisesController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CSharpTeacher.Models;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System.Threading.Tasks;
 
 namespace CSharpTeacher.Controllers
 {
@@ -13,5 +18,62 @@ namespace CSharpTeacher.Controllers
         {
             return View();
         }
+        [HttpGet]
+        public ActionResult Exercise1(Exercise1ViewModel model, int? a)
+        {
+            model.expression = "string metoda(****** napis) { ****** napis; } metoda(\"hehe\")";
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public ActionResult Exercise1(Exercise1ViewModel model)
+        {
+            CsharpResult(model);
+
+            //Do osobnej metody
+            if (string.Equals(model.result, "hehe"))
+            {
+                model.sysAnswer = "Gratulacje !";
+                model.isExerciseDone = true;
+            }
+
+            else if (!string.Equals(model.result, "hehe")) { model.sysAnswer = "Źle !"; }
+            //
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Exercise2(Exercise1ViewModel model, int? a)
+        {
+            model.expression = "string metoda(****** napis) { ****** napis; } metoda(\"hehe\")";
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Exercise2(Exercise1ViewModel model)
+        {
+            CsharpResult(model);
+
+            //Do osobnej metody
+            if (int.Equals(model.result, 50))
+            {
+                model.sysAnswer = "Gratulacje !";
+                model.isExerciseDone = true;
+            }
+
+            else if (!int.Equals(model.result, 50)) { model.sysAnswer = "Źle !"; }
+            //
+
+            return View(model);
+        }
+
+        private async Task CsharpResult(Exercise1ViewModel model)
+        {
+            model.result = await CSharpScript.EvaluateAsync(model.expression);
+        }
+
+
     }
 }
---
+++ CSharpTeacher/Models/Exercise1ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSharpTeacher.Models
+{
+    public class Exercise1ViewModel
+    {
+        public string expression { get; set; }
+        public string sysAnswer { get; set; }
+        public bool isExerciseDone { get; set; } = false;
+        public object result { get; set; }
+    }
+}
